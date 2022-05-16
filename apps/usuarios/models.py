0c7f2e0c3f5829e9bb6dff3ac4743787501a97a5
KevinMartinezMella <iamkamm7@gmail.com>
2022-05-16 02:56:50 -0400

    Portada BuffedRiven

---
+++ apps/usuarios/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length = 50, null = False, blank = False)
+    apellido = models.CharField(max_length = 50, null = False, blank = False)
+    email = models.CharField(max_length = 255, null = False, blank = False)
+    password = models.CharField(max_length = 70, null = False, blank = False)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
