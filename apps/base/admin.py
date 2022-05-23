5237e1ce6bfa1b13904de9be245afa15635ff502
KevinMartinezMella <iamkamm7@gmail.com>
2022-05-23 14:04:59 -0400

    Creacion de modelos

--- apps/base/admin.py
+++ apps/base/admin.py
@@ -1,3 +1,12 @@
+import imp
 from django.contrib import admin
+from . models import Division, Usuario, Rango, Servidor, Cuenta
 
 # Register your models here.
+
+
+admin.site.register(Usuario)
+admin.site.register(Rango)
+admin.site.register(Servidor)
+admin.site.register(Cuenta)
+admin.site.register(Division)
--- apps/base/models.py
+++ apps/base/models.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from django.db import models
 
 class Usuario(models.Model):
@@ -7,3 +8,51 @@ class Usuario(models.Model):
     password = models.CharField(max_length = 70, null = False, blank = False)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return f'{self.nombre}'
+
+    class Meta:
+        db_table = 'usuario'
+
+class Division(models.Model):
+    numero_division = models.CharField(max_length = 10, null = True)
+
+    def __str__(self):
+            return f'{self.numero_division}'
+
+    class Meta:
+        db_table = 'division'
+
+class Rango(models.Model):
+    nombre_rango = models.CharField(max_length = 255, null = False)
+    
+    def __str__(self):
+            return f'{self.nombre_rango}'
+
+    class Meta:
+        db_table = 'rango'
+
+class Servidor(models.Model):
+    nombre_servidor = models.CharField(max_length = 255, null = False)
+
+    def __str__(self):
+        return f'{self.nombre_servidor}'
+
+    class Meta:
+        db_table = 'servidor'
+
+class Cuenta(models.Model):
+    nombre_usuario = models.CharField(max_length = 50, null = False)
+    password = models.CharField(max_length = 70, null = False)
+    nivel = models.IntegerField(null = False)
+    escencia = models.IntegerField(null = True)
+    rango = models.ForeignKey(Rango, on_delete = models.PROTECT)
+    division = models.ForeignKey(Division, null = True, on_delete = models.PROTECT)
+    servidor = models.ForeignKey(Servidor, on_delete = models.PROTECT)
+
+    def __str__(self):
+        return f'{self.nombre_usuario}'
+
+    class Meta:
+        db_table = 'cuenta'
